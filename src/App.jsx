903c5c9d3274c6b9d6617edac7438352f0a33f64
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2026-02-11 07:55:36 +0530

    Added changes related to css modules and also added global css for the application

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from "./pages/Homepage.jsx"
 import Pricing from "./pages/Pricing.jsx"
 import Product from "./pages/Product.jsx"
 import PageNotFound from "./pages/PageNotFound.jsx"
+import AppLayout from "./pages/AppLayout.jsx"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom"
 import PageNav from "../components/PageNav.jsx"
+import AppNav from "../components/AppNav.jsx"
+
 
 
 function Homepage() {
     return (
         <div>
             <PageNav />
+            <AppNav />
             <h1>Worldwise</h1>
+            <Link to="/app">Go to App</Link>
         </div>
     )
 }
