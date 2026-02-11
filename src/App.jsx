27f249e9ceda6685113dbee34e7f13fae984163b
Suraj Ingole <suraj.ingole0610@gmail.com>
2026-02-11 12:46:32 +0530

    Refactor routing structure and enhance navigation components for improved SPA functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/app" element={<AppLayout />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route index element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
         <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} >
+          <Route index element={<p>List </p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,15 +2,14 @@
 import AppNav from "./AppNav";
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
     return (
         <div className={styles.sidebar}>
             <Logo />
             <AppNav />
-            <p>
-                List of cities
-            </p>
+            <Outlet />
             <footer className={styles.footer}>
                 <p className={styles.copyright}>
                     &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
