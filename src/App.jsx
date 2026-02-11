643bbbcda92b33a160812abe1a060b9689d91b6c
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2026-02-11 08:18:39 +0530

    Added few additional files for the application and worked on Page navigations. Added clear data and template for the pages and component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Pricing from "./pages/Pricing.jsx"
 import Product from "./pages/Product.jsx"
 import PageNotFound from "./pages/PageNotFound.jsx"
 import AppLayout from "./pages/AppLayout.jsx"
+import Login from "./pages/Login.jsx"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/product" element={<Product />} />
         <Route path="/app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,11 +1,7 @@
-import PageNav from "../components/PageNav.jsx"
-function PageNotFound() {
-    return (
-        <div>
-            <PageNav />
-            <h1>Page Not Found :(</h1>
-        </div>
-    )
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
 }
-
-export default PageNotFound
