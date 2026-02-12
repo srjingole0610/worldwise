89415c0129e3407483f8bb399469110510d3402a
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2026-02-12 21:44:10 +0530

    feat: implement CitiesContext for centralized city data management and refactor CityList and CountryList components

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,9 +2,11 @@ import styles from './CountryList.module.css'
 import Spinner from './Spinner'
 import Message from './Message'
 import CountryItem from './CountryItem'
+import { useCities } from '../contexts/CitiesContext.jsx'
 
 import PropTypes from 'prop-types'
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+    const { cities, isLoading } = useCities()
     if (isLoading) return <Spinner />
 
     if (!cities.length) return <Message message="Add Your first city by clicking on a city on the map" />
